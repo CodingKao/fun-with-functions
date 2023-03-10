e295747924ce0430c6f616782dfd7e716f3128a5
Kao Saelor <saelor.kao@gmail.com>
2023-03-10 14:29:48 -0800

    sum

--- js/app.js
+++ js/app.js
@@ -9,15 +9,14 @@ Test this function by hand in the console to get it working, and when you think 
 
 // Write your code here
 function sum(a, b) { //eslint-disable-line
-    let a = 3;
-    let b = 4;
-    }
-    sum();
-    console.log(sum);
-    alert("The sum of " + a + " and" + b + "is "+ sum);
+    let mySum = a + b;
+    let sentence = `The sum of ${a} and ${b} is ${mySum}.`;
+    return [mySum, sentence];
+    // return [a+b, `The sum of ${a} and ${b} is ${a+b}.`];
+}
 
 // Here is the test for sum(); uncomment it to run it
-// testSum(4, 7);
+testSum(4, 7);
 
 // Once you get the test passing, do an a-c-p cycle and synchronize the code between GitHub and your laptop. Don't forget to create a new branch for your work on the next question!
 
